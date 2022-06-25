25c0af6b8831296354b378d3aca5ecf3d4c87a17
nao <nurettinakinoral@gmail.com>
2022-06-25 11:29:48 +0300

    Fonts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome React</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem enim
+        culpa, ab voluptas velit rerum tempora expedita quia tempore atque?
+        Quibusdam doloribus tempore sequi ea? Fugiat modi nihil accusamus esse.
+      </p>
     </div>
   );
-}
+};
 
 export default App;
