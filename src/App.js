8455da8588861a0fa5aa2cd1d90506d0e771da31
nao <nurettinakinoral@gmail.com>
2022-06-25 11:42:35 +0300

    Navbar end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome React</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem enim
-        culpa, ab voluptas velit rerum tempora expedita quia tempore atque?
-        Quibusdam doloribus tempore sequi ea? Fugiat modi nihil accusamus esse.
-      </p>
+      <Navbar />
     </div>
   );
 };
